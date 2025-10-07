420c4c05ffa3ce6f73d8eaf8eff0172fcb71fc46
Rishabh Katna <rishabhkatna@Rishabhs-MacBook-Air.local>
2025-10-07 20:43:09 +0530

    added Readme for packages/ui

---
+++ packages/ui/components/index.tsx
@@ -0,0 +1,7 @@
+export { default as Box } from "./Box";
+export { default as Text } from "./Text";
+export { default as Stack } from "./Stack";
+export { default as Modal } from "./Modal";
+export { default as Card } from "./Card";
+export { default as Input } from "./Input";
+export { default as Button } from "./Button";
